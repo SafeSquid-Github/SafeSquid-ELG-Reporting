22d5e6b32c3eb2830b9b06f0a1d30974c9ed6b5a
Pratik <pratikpoojary124@outlook.com>
2025-03-17 16:24:21 +0530

    Refactor setup script to streamline repository addition and enhance Grafana datasource configuration

--- setup.sh
+++ setup.sh
@@ -3,22 +3,37 @@
 # Exit on error
 set -e
 
+ELASTIC_REPO_URL="https://artifacts.elastic.co/packages/8.x/apt"
+GRAFANA_REPO_URL="https://packages.grafana.com"
+
+
 UPDATE_PACKAGES_GET_DEPENDENCIES () 
 {
     sudo apt update && sudo apt upgrade -y
     sudo apt install -y apt-transport-https wget curl gnupg
 }
 
+ADD_REP_ELASTIC () 
+{
+    wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch gpg --dearmor | tee /etc/apt/keyrings/elasticsearch-keyring.gpg > /dev/null
+    sudo echo "deb [signed-by=/etc/apt/keyrings/elasticsearch-keyring.gpg] ${ELASTIC_REPO_URL} stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
+}
+
+APP_REPO_GRAFANA ()
+{
+    wget -qO - https://apt.grafana.com/gpg.key | gpg --dearmor | tee /etc/apt/keyrings/grafana.gpg > /dev/null
+    sudo echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] ${GRAFANA_REPO_URL} stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+}
+
 ADD_REPO () 
 {
-    # Add Elasticsearch repository
-    wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
-    echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
-    
-    # Add Grafana repository
     sudo mkdir -p /etc/apt/keyrings/
-    wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | tee /etc/apt/keyrings/grafana.gpg > /dev/null
-    sudo echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+    apt-cache policy | awk '/http.*amd64/{print$2}' | sort -u | while read -r REPO_URL; do
+        # Add Elasticsearch repository
+        [[ $REPO_URL != ${ELASTIC_REPO_URL} ]] && ADD_REP_ELASTIC
+        # Add Grafana repository
+        [[ $REPO_URL != ${GRAFANA_REPO_URL} ]] && APP_REPO_GRAFANA
+    done 
 }
 
 INSTALL () 
@@ -40,7 +55,7 @@ CONFIGURE_GRAFANA_DATASOURCES ()
 {
     echo "Configuring Grafana data sources..."
     curl -X POST -H "Content-Type: application/json" -d '{
-        "name": "SafeSquid-Config",
+        "name": "safesquid-config",
         "type": "elasticsearch",
         "url": "http://localhost:9200",
         "access": "proxy",
@@ -50,10 +65,10 @@ CONFIGURE_GRAFANA_DATASOURCES ()
             "esVersion": 70,
             "timeField": "@timestamp"
         }
-    }' http://admin:admin@localhost:3000/api/datasources
+    }' http://admin:sarva1234@localhost:3000/api/datasources
 
     curl -X POST -H "Content-Type: application/json" -d '{
-        "name": "SafeSquid-CSP",
+        "name": "safesquid-csp",
         "type": "elasticsearch",
         "url": "http://localhost:9200",
         "access": "proxy",
@@ -63,7 +78,7 @@ CONFIGURE_GRAFANA_DATASOURCES ()
             "esVersion": 70,
             "timeField": "@timestamp"
         }
-    }' http://admin:admin@localhost:3000/api/datasources
+    }' http://admin:sarva1234@localhost:3000/api/datasources
 
     curl -X POST -H "Content-Type: application/json" -d '{
         "name": "safesquid-extended",
@@ -76,7 +91,7 @@ CONFIGURE_GRAFANA_DATASOURCES ()
             "esVersion": 70,
             "timeField": "@timestamp"
         }
-    }' http://admin:admin@localhost:3000/api/datasources
+    }' http://admin:sarva1234@localhost:3000/api/datasources
 
     curl -X POST -H "Content-Type: application/json" -d '{
         "name": "safesquid-performance",
@@ -89,16 +104,39 @@ CONFIGURE_GRAFANA_DATASOURCES ()
             "esVersion": 70,
             "timeField": "@timestamp"
         }
-    }' http://admin:admin@localhost:3000/api/datasources
+    }' http://admin:sarva1234@localhost:3000/api/datasources
+}
+
+UPDATE_CONF () 
+{
+    declare -A UID_MAP
+    for JSON_FILE in grafana_dashboard/*; 
+    do   
+        LABELS=$(jq -r '.dashboard.__inputs[] | .label' "$JSON_FILE")
+        if [ "$LABELS" != "safesquid-performance" ]; 
+        then
+            RELATIVE_UID=$(jq -r '.dashboard.templating.list[0].datasource.uid' "$JSON_FILE")
+            ABSOLUTE_UID=$(curl -s http://admin:sarva1234@localhost:3000/api/datasources/name/$LABELS | jq -r '.uid')
+            echo $RELATIVE_UID : $ABSOLUTE_UID
+            [[ ${RELATIVE_UID} == ${ABSOLUTE_UID} ]] && continue
+            sed -i "s/\"uid\": \"$RELATIVE_UID\"/\"uid\": \"$ABSOLUTE_UID\"/g" "$JSON_FILE"
+        else 
+            RELATIVE_UID=$(jq -r '.dashboard.panels[1] | .datasource.uid' "$JSON_FILE")
+            ABSOLUTE_UID=$(curl -s http://admin:sarva1234@localhost:3000/api/datasources/name/$LABELS | jq -r '.uid')
+            echo $RELATIVE_UID : $ABSOLUTE_UID
+            [[ ${RELATIVE_UID} == ${ABSOLUTE_UID} ]] && continue
+            sed -i "s/\"uid\": \"$RELATIVE_UID\"/\"uid\": \"$ABSOLUTE_UID\"/g" "$JSON_FILE"
+        fi
+    done 
 }
 
 IMPORT_GRAFANA_DASHBOARD () 
 {
     #Import dasboard to grafana
-    curl -X POST -H "Content-Type: application/json" -d @grafana_dashboard/configuration_portal_reports.json http://admin:admin@localhost:3000/api/dashboards/import
-    curl -X POST -H "Content-Type: application/json" -d @grafana_dashboard/content_security_policy_reports.json http://admin:admin@localhost:3000/api/dashboards/db
-    curl -X POST -H "Content-Type: application/json" -d @grafana_dashboard/performance_plots.json http://admin:admin@localhost:3000/api/dashboards/db
-    curl -X POST -H "Content-Type: application/json" -d @grafana_dashboard/user_reports.json http://admin:admin@localhost:3000/api/dashboards/db
+    curl -X POST -H "Content-Type: application/json" -d @grafana_dashboard/performance_plots.json http://admin:sarva1234@localhost:3000/api/dashboards/db
+    curl -X POST -H "Content-Type: application/json" -d @grafana_dashboard/user_reports.json http://admin:sarva1234@localhost:3000/api/dashboards/db
+    curl -X POST -H "Content-Type: application/json" -d @grafana_dashboard/content_security_policy_reports.json http://admin:sarva1234@localhost:3000/api/dashboards/db
+    curl -X POST -H "Content-Type: application/json" -d @grafana_dashboard/configuration_portal_reports.json http://admin:sarva1234@localhost:3000/api/dashboards/import
 }
 
 MAIN () 
@@ -108,6 +146,7 @@ MAIN ()
     INSTALL && echo "Installation complete! Elasticsearch, Logstash, and Grafana are running."
     SETUP_CONF
     CONFIGURE_GRAFANA_DATASOURCES
+    UPDATE_CONF
     IMPORT_GRAFANA_DASHBOARD
     systemctl restart grafana-server.service
 }
